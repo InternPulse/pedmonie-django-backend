0ef93c48d8eaf800626d78b6df20ef7a5b1daf14
KC_software_en <kisha.cairncross@gmail.com>
2025-02-13 23:14:59 +0200

    registered Merchant in authentication/admin.py (not the helper class MerchantManager)

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+#########################################################################
+
+# register Merchant model
+# NOTE: don't register MerchantManager
+# - it's not a model but a (helper) custom manager class that handles the creation of user instances
+admin.site.register(Merchant)
