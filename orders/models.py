db892204a38a143c983670ff5bff115a677a86bc
Dikachi J <dikachimichael253@gmail.com>
2025-02-16 21:08:12 +0100

    created order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from authentication.models import Merchant
+import uuid
 
-# Create your models here.
+class Order(models.Model):
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    gateway_name = models.CharField(max_length=50)
+    merchant_id = models.ForeignKey(Merchant, on_delete=models.PROTECT, related_name='orders')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3)
+    order_status = models.CharField(max_length=20,default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
