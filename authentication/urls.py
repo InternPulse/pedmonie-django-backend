3249f5c628d2b72c88e5aa2bee9bde06e9e882cb
KC_software_en <kisha.cairncross@gmail.com>
2025-02-16 19:30:32 +0200

    added post & get ulrs to views in authentication/urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,15 @@
+# import path for URL pattern routing in Django
 from django.urls import path
-from .views import MerchantCreateView
 
+# import AdminView class from views.py in the current directory
+from .views import AdminView
+
+########################################################################################################################
+
+# add url to superuser post method
+# add url to superuser get method
+# don't specify method -> APIView automatically maps HTTP methods (POST, GET) to corresponding class methods with as_view()
 urlpatterns = [
-    path('api/v1/merchants', MerchantCreateView.as_view(), name='create-merchant'),
+    path('api/v1/admins/', AdminView.as_view(), name='create_superuser'), 
+    path('api/v1/admins/<uuid:merchant_id>/', AdminView.as_view(), name='get_superuser'),    
 ]
