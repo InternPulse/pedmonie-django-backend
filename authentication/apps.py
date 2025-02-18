200dc62a29520cfbdbcd3bd0c9740895bb2f039c
mantle-bearer <igbokwegoodluck8@gmail.com>
2025-02-18 20:03:25 +0100

    refactor: remove test email logic and delete unused migration files

--- authentication/apps.py
+++ authentication/apps.py
@@ -6,7 +6,4 @@ class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
 
-    def ready(self):
-        """Run email test when the server starts."""
-        print("Server restarted - Sending test email...")
-        send_test_email()
+   
