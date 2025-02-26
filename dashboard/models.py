86eb62a52d5b6089598d9f10cdfeb708eac18dc7
Goodluck Chigozie <igbokwegoodluck8@gmail.com>
2025-02-26 04:22:42 -0800

    Refactor Serial Number field to use AutoField for consistency across models

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,24 +4,12 @@ from authentication.models import Merchant
 
 class AuditLog(models.Model):
     log_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    sn = models.CharField(max_length=50,unique=True, db_index=True, verbose_name="Serial Number", blank=True)
+    sn = models.AutoField(unique=True, db_index=True, verbose_name="Serial Number")
     admin = models.ForeignKey(Merchant, on_delete=models.CASCADE, limit_choices_to={'role': 'superadmin'})  # Reference Merchant model
     action = models.TextField()  # Example: "Deleted a merchant account"
     createdAt = models.DateTimeField(auto_now_add=True)
     class Meta:
         db_table = 'auditlog'
-
-    
-    def save(self, *args, **kwargs):
-        if not self.sn:  # Only assign if 'sn' is empty
-            last_log = AuditLog.objects.exclude(sn='').order_by(models.functions.Cast('sn', models.IntegerField()).desc()).first()
-            if last_log and last_log.sn.isdigit():
-                self.sn = str(int(last_log.sn) + 1)
-            else:
-                self.sn = "1"  # Start from 1 if no records exist
-        super().save(*args, **kwargs)
-
-    
 
     
     def __str__(self):
--- transactions/models.py
+++ transactions/models.py
@@ -7,7 +7,7 @@ from orders.models import Order
 
 class Transaction(models.Model):
     transaction_id = models.UUIDField(primary_key=True)
-    sn = models.CharField(max_length=50,unique=True, db_index=True, verbose_name="Serial Number", blank=True)
+    sn = models.AutoField(unique=True, db_index=True, verbose_name="Serial Number")
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_transactions')
     merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name='transactions')
     gateway_name = models.CharField(max_length=50)
@@ -28,13 +28,5 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"Transaction {self.transaction_id} - {self.status}"
-    
-    def save(self, *args, **kwargs):
-        if not self.sn:  # Only assign if 'sn' is empty
-            last_transaction = Transaction.objects.exclude(sn='').order_by(models.functions.Cast('sn', models.IntegerField()).desc()).first()
-            if last_transaction and last_transaction.sn.isdigit():
-                self.sn = str(int(last_transaction.sn) + 1)
-            else:
-                self.sn = "1"  # Start from 1 if no records exist
-        super().save(*args, **kwargs)
+
         
