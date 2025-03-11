c8908d12aa1f786a7d38b29cbd18192c4421682d
Onome <Onome@Onomes-Macbook-Pro.local>
2025-03-11 22:49:49 +0000

    Added cors origin

--- pedmonie/settings.py
+++ pedmonie/settings.py
@@ -281,7 +281,12 @@ LOGGING = {
     },
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5174", 
+]   
 CSRF_TRUSTED_ORIGINS = [
     "https://pedmonie-django-backend.onrender.com"
 ]
