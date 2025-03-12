667225703e42be371e3c05078e3506c3b2f41088
Onome <Onome@onomes-mbp.home>
2025-03-12 07:40:14 +0000

    added CORS ALLOW CREDENTIALS

--- pedmonie/settings.py
+++ pedmonie/settings.py
@@ -287,6 +287,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174", 
 ]   
+
+CORS_ALLOW_CREDENTIALS = True
+
 CSRF_TRUSTED_ORIGINS = [
     "https://pedmonie-django-backend.onrender.com"
 ]
